75f53bbf58cd267dc8a7237498211645342c928f
wlad111 <wlad962961@gmail.com>
2017-10-30 22:23:24 +0300

    untracking

--- NewtonOptimizer.cpp
+++ NewtonOptimizer.cpp
@@ -21,17 +21,17 @@ VectorXd NewtonOptimizer::optim(const VectorXd &x0, double eps, const AbstractFu
 	VectorXd x_next = H.colPivHouseholderQr().solve(H*x0 - f.gradient(x0));
 	VectorXd x_prev = x0;
 	int i = 1;
-	//cout << i << "Iteration" << endl;
-	//cout << x_next << endl;
+	cout << i << "Iteration" << endl;
+	cout << x_next << endl;
 	while (stop.criterion(x_next, x_prev, eps, f))
 	{
-		H = f.hessian(x0);
+		H = f.hessian(x_prev);
 		if ((H(0, 0) < 0) || (H(0, 0)*H(1, 1) - H(0, 1)*H(1, 0) < 0))
 		{
 			cout << "Negative" << endl;
 		}
 		x_prev = x_next;
-		x_next = H.colPivHouseholderQr().solve(H*x0 - f.gradient(x0));
+		x_next = H.colPivHouseholderQr().solve(H*x_prev - f.gradient(x_prev));
 		for (int i = 0; i < f.getDim(); i++)
 		{
 			if ((x_next(i)) < f.getArea().getLeft()(i))
